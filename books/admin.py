e2e72c0fc563e12afc2ce45c4a1b2445a318e7ff
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-05-03 14:35:04 +0000

    Register the book model/data to the admin

--- books/admin.py
+++ books/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Book
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "book_id",
+	    "title",
+	    "author",
+	    "price",
+	)
+    ordering = ("book_id",)
+
+admin.site.register(Book, BookAdmin)
