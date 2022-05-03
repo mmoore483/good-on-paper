5869cadcf9d09e37eb43c5abf62a8e8d8d0eb69c
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-05-03 18:43:25 +0000

    Added book and book_detail templates, views, urls

--- books/views.py
+++ books/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
-# Create your views here.
+
+def all_books(request):
+    """ a view to show all books, including sorting and search queries """
+    books = Book.objects.all()
+    context = {
+        "books": books,
+    }
+    return render(request, 'books/book.html', context)
+
+
+def book_detail(request, book_id):
+    """ A view to show individual book details """
+
+    book = get_object_or_404(Book, pk=book_id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'books/book_detail.html', context)
