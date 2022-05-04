eb91a26bfe1e5b8609676cd5f15b6e984afffd1f
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-05-04 08:42:05 +0000

    Add product sorting and filtering to nav links

--- books/views.py
+++ books/views.py
@@ -7,7 +7,28 @@ def all_books(request):
     """ a view to show all books, including sorting and search queries """
     books = Book.objects.all()
     query = None
+    sort = None
+    direction = None
+
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'price':
+                sortkey = 'price'
+            if sortkey == 'rating':
+                sortkey = 'rating'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            books = books.order_by(sortkey)
+
+        if 'genres' in request.GET:
+            genresq = request.GET['genres']
+            books = books.filter(Q(genres__icontains=genresq))
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -16,9 +37,12 @@ def all_books(request):
             queries = Q(author__icontains=query) | Q(description__icontains=query) | Q(
                 genres__icontains=query) | Q(isbn__icontains=query) | Q(series__icontains=query)
             books = books.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
     context = {
         "books": books,
         "search_term": query,
+        "current_sorting": current_sorting,
     }
 
     return render(request, 'books/book.html', context)
