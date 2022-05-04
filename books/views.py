e9f018abdf54f618d62616da0efb3df4278ccd1e
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-05-04 07:01:00 +0000

    Add book querying to all_books view

--- books/views.py
+++ books/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.db.models import Q
 
 
 def all_books(request):
     """ a view to show all books, including sorting and search queries """
     books = Book.objects.all()
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('books'))
+            queries = Q(author__icontains=query) | Q(description__icontains=query) | Q(
+                genres__icontains=query) | Q(isbn__icontains=query) | Q(series__icontains=query)
+            books = books.filter(queries)
     context = {
         "books": books,
+        "search_term": query,
     }
+
     return render(request, 'books/book.html', context)
 
 
