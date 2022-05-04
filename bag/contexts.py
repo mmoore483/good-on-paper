5bfedb7f42501bfa6136d4dec9aa57605704d5f5
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-05-04 13:25:24 +0000

    Update styling of book and bag templates

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,8 @@
 # This code is from the Boutqiue-Ado WalkThrough from the Code Institute
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from books.models import Book
 
 
 def bag_contents(request):
@@ -8,6 +10,18 @@ def bag_contents(request):
     bag_items = []
     total = 0
     book_count = 0
+    bag = request.session.get('bag', {})
+
+
+    for item_id, quantity in bag.items():
+        book = get_object_or_404(Book, pk=item_id)
+        total += quantity * book.price
+        book_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'book': book,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
