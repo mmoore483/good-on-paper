1a2225f1a81e6c011f7370ebc86d05edba6e544c
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-05-05 13:33:51 +0000

    Add order model to admin and set up signals and apps.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'created_on',
+                       'delivery_cost', 'order_total',
+                       'grand_total')
+
+    fields = ('order_number', 'customer_profile', 'created_on', 'delivery_cost',
+              'order_total', 'grand_total')
+
+    list_display = ('order_number', 'created_on', 'customer_profile',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-created_on',)
+
+
+admin.site.register(Order, OrderAdmin)
