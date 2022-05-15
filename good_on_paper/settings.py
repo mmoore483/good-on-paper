d626c74606d339d8f8ebd639fb758febf0b439d1
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-05-15 17:36:51 +0000

    Change debug to false

--- good_on_paper/settings.py
+++ good_on_paper/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mm483-good-on-paper.herokuapp.com', 'localhost']
 
